32130ae0be4b3484f0d0bcf611687440692bea98
Ian McKenzie <imckenzie777@gmail.com>
2020-03-09 10:26:00 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,41 @@
+chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  let newArray = [...array.slice(0)];
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  let newArray = array;
+  array.push(element);
+  return newArray;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, 2);
+}
